21dc53b75bd47f5e3db04bc9425030d77a762055
Haris Patsouras <haris.p.dev@gmail.com>
2025-11-19 20:20:17 +0200

    Added do while for  input control

--- Main.java
+++ Main.java
@@ -6,29 +6,35 @@ public class Main {
         Scanner reader = new Scanner(System.in);
 
         String init;
+        char firstLetter ;
 
         System.out.print("Welcome to the weight converter. \n");
-        System.out.print("Press 'K' to convert kilos to lbs. \n");
-        System.out.print("Or press 'L' to convert lbs to kilos. \n");
 
-        init = reader.next();
-        char firstLetter = init.toUpperCase().charAt(0);
+        do {
+            System.out.print("Press 'K' to convert kilos to lbs. \n");
+            System.out.print("Or press 'L' to convert lbs to kilos. \n");
+            init = reader.next();
+            firstLetter = init.toUpperCase().charAt(0);
 
-        if (firstLetter == 'K') {
+            if(firstLetter!='K' && firstLetter!='L'){
+                System.out.println("Invalid option. Try again.");
+            }
+        }
+        while (firstLetter!='K' && firstLetter!='L');
+
+         if (firstLetter == 'K') {
             double kilos;
             System.out.print("Enter the amount of kilos you would like to convert: ");
             kilos = reader.nextDouble();
             double lbs = kilos * 2.20462;
             System.out.printf(" %.2f kilos is %.2f lbs.", kilos, lbs);
-        } else if (firstLetter == 'L') {
+        } else {
             double lbs;
             System.out.print("Enter the amount of lbs you would like to convert: ");
             lbs = reader.nextDouble();
             double kilos = lbs * 0.453592;
             System.out.printf(" %.2f lbs is %.2f kilos.", lbs, kilos);
 
-        } else {
-            System.out.println("Please enter a valid option.");
         }
     }
 }
